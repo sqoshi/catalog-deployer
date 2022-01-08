cfdd756b8d81e2c52cdbf9361a7128e07b65d27c
popis <piotr.popis@nokia.com>
2022-01-08 16:51:39 +0100

    Directory scanning utilities

---
+++ main.go
@@ -0,0 +1,14 @@
+//asdaasd
+//
+// Introduction
+//
+// This Program shares the content of directory from given system path via API.
+//
+package main
+
+import "proj1/systemscanner"
+
+func main() {
+	path := "./test_dir1"
+	systemscanner.ReadEntry(path)
+}
